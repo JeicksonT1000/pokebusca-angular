ec97fb2c9e30d714a2440f4256342ae96bab6b9a
Bruno Modinuti <modinutidev@gmail.com>
2022-11-09 19:11:35 -0300

    Adiciona todos os componentes necessários

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CabecalhoComponent } from './components/lista/cabecalho/cabecalho.component';
+import { ListaComponent } from './components/lista/lista.component';
+import { ContadorComponent } from './components/lista/contador/contador.component';
+import { ItemComponent } from './components/lista/item/item.component';
+import { PaginacaoComponent } from './components/lista/paginacao/paginacao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CabecalhoComponent,
+    ListaComponent,
+    ContadorComponent,
+    ItemComponent,
+    PaginacaoComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
