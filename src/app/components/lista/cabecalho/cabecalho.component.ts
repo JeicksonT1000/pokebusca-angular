4d42942569f485045e52c2c0eabef54b9a97245d
Bruno Modinuti <modinutidev@gmail.com>
2022-11-11 01:06:23 -0300

    Adicionado logica no componente Cabecalho

--- src/app/components/lista/cabecalho/cabecalho.component.ts
+++ src/app/components/lista/cabecalho/cabecalho.component.ts
@@ -1,21 +1,35 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-cabecalho',
   templateUrl: './cabecalho.component.html',
   styleUrls: ['./cabecalho.component.scss'],
 })
-export class CabecalhoComponent implements OnInit {
+export class CabecalhoComponent {
+  constructor(private api: ApiService) {}
+
+  @Output() eventBuscar: EventEmitter<any> = new EventEmitter();
+
   titulo: string = 'Fase do teste prático Front-end';
   subtitulo: string = 'Pokebusca';
   termoBusca: string = '';
+  itemBusca: any = {};
 
-  buscarPokemon() {
-    // Api
-    console.log(this.termoBusca);
-  }
-
-  ngOnInit(): void {
-    console.log('Inicio');
+  async buscarPokemon() {
+    if (this.termoBusca.length > 0) {
+      await this.api
+        .buscarPokemon(this.termoBusca)
+        .then((res) => {
+          this.itemBusca = res;
+          this.eventBuscar.emit(this.itemBusca);
+        })
+        .catch(() => {
+          this.itemBusca = {};
+          this.eventBuscar.emit(this.itemBusca);
+        });
+    } else {
+      this.eventBuscar.emit({});
+    }
   }
 }
