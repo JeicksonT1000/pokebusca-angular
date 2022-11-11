acc97a4ea309ae9384b4775e9b14085600b345b5
Bruno Modinuti <modinutidev@gmail.com>
2022-11-11 01:05:34 -0300

    Adicionado logica no componente Item

--- src/app/components/lista/item/item.component.ts
+++ src/app/components/lista/item/item.component.ts
@@ -1,18 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.scss'],
 })
-export class ItemComponent implements OnInit {
-  nome: string = 'Nome do pokemon';
-  urlImg: string =
-    'https://assets.pokemon.com/static2/_ui/img/og-default-image.jpeg';
-  descricao: string =
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, nostrum blanditiis earum consequatur laboriosam reiciendis dolor, saepe repellendus, numquam porro deserunt aspernatur! Magni delectus cupiditate nobis alias consequatur similique earum?';
-
+export class ItemComponent {
   constructor() {}
 
-  ngOnInit(): void {}
+  @Input() id: number = 0;
+  @Input() nome: string = '';
+  @Input() urlImg: string = '';
+  descricao: string =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, nostrum blanditiis earum consequatur laboriosam reiciendis dolor, saepe repellendus, numquam porro deserunt aspernatur! Magni delectus cupiditate nobis alias consequatur similique earum?';
 }
