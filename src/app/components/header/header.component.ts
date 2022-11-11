07644772064972fb52c62b6d2a6545010495ddcd
Bruno Modinuti <modinutidev@gmail.com>
2022-11-11 01:07:19 -0300

    Ajustado componente Header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
   nomeUsuario: string = 'Bruno Modinuti';
   urlLogo: string = '../../../assets/logo_advise.svg';
   nomeEmpresa: string = 'Advise';
   urlSiteEmpresa: string = 'https://advise.com.br/';
-
-  constructor() {}
-
-  ngOnInit(): void {}
 }
