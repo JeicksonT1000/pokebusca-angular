0c47433510bc34a5e88bf62ff221b4dd7457f11d
Bruno Modinuti <modinutidev@gmail.com>
2022-11-11 01:05:12 -0300

    Adicionado logica no componente Paginacao

--- src/app/components/lista/paginacao/paginacao.component.ts
+++ src/app/components/lista/paginacao/paginacao.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-paginacao',
   templateUrl: './paginacao.component.html',
-  styleUrls: ['./paginacao.component.scss']
+  styleUrls: ['./paginacao.component.scss'],
 })
-export class PaginacaoComponent implements OnInit {
+export class PaginacaoComponent {
+  constructor() {}
 
-  constructor() { }
+  @Output() navPage: EventEmitter<any> = new EventEmitter();
 
-  ngOnInit(): void {
+  pagina: number = 1;
+  total: number = 4;
+
+  voltar() {
+    this.pagina > 1 ? this.pagina-- : null;
+    this.navPage.emit(this.pagina);
+  }
+  avancar() {
+    this.pagina < this.total ? this.pagina++ : null;
+    this.navPage.emit(this.pagina);
   }
 
+  primeira() {
+    this.pagina = 1;
+    this.navPage.emit(this.pagina);
+  }
+
+  ultima() {
+    this.pagina = this.total;
+    this.navPage.emit(this.pagina);
+  }
 }
