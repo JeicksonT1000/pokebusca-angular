86600967c4fdf85099e59f87b9eb3b49a3a88cd0
Bruno Modinuti <modinutidev@gmail.com>
2022-11-11 01:06:40 -0300

    Adicionado logica no componente Lista

--- src/app/components/lista/lista.component.ts
+++ src/app/components/lista/lista.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html',
-  styleUrls: ['./lista.component.scss']
+  styleUrls: ['./lista.component.scss'],
 })
 export class ListaComponent implements OnInit {
+  constructor(private api: ApiService) {}
 
-  constructor() { }
+  listaPokemons: Array<any> = [];
+  // Controle de itens exibidos
+  pagina: number = 1;
+  limiteItens: number = 5;
+  totalItens: number = 0;
+  itemControle: number = this.pagina * this.limiteItens;
+  itemBusca: any = {};
 
-  ngOnInit(): void {
+  atualizaPagina(pagina: number) {
+    this.pagina = pagina;
+    this.itemControle = this.pagina * this.limiteItens;
   }
 
+  onNavPage(event: any) {
+    this.atualizaPagina(event);
+  }
+
+  onEventBuscar(event: any) {
+    this.itemBusca = event;
+  }
+
+  async ngOnInit() {
+    await this.api.montaListaPokemons().then((res) => {
+      this.listaPokemons = res;
+    });
+  }
 }
