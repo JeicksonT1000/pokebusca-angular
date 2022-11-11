1426a58566bb4260952286994c0c88380d535bf0
Bruno Modinuti <modinutidev@gmail.com>
2022-11-11 01:05:52 -0300

    Adicionado logica no componente Contador

--- src/app/components/lista/contador/contador.component.ts
+++ src/app/components/lista/contador/contador.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-contador',
   templateUrl: './contador.component.html',
-  styleUrls: ['./contador.component.scss']
+  styleUrls: ['./contador.component.scss'],
 })
-export class ContadorComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ContadorComponent {
+  @Input() totalItens: number = 0;
+  @Input() limiteItens: number = 0;
 }
